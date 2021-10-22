22eef2f02b4e9735a4f47eb53c49e369130d410a
jtmk4752 <jtmk4752@yahoo.co.jp>
2021-10-22 11:18:56 +0900

    test

--- server.py
+++ server.py
@@ -44,9 +44,9 @@ class SocketServer():
                 rcv_data = client_socket.recv(DATESIZE)
                 rcv_data_decode = rcv_data.decode()
 #                rcv_data_decode = rcv_data.decode("utf-8")
-                if int(rcv_data_decode) > 10:
+                if int.from_bytes(rcv_data_decode) > 10:
                     # データ受信したデータをそのままクライアントへ送信
-                    client_socket.send(1)
+                    client_socket.send(rcv_data_decode)
                     print('[{0}] recv date : {1}'.format(datetime.now().strftime(
                         '%Y-%m-%d %H:%M:%S'), rcv_data_decode))
                 else:
