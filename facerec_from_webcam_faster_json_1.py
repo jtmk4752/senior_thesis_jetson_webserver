f635781b8bdc062a4d40204d5c8205f029782e6b
jtmk4752 <jtmk4752@yahoo.co.jp>
2021-10-23 14:21:07 +0900

    adding facerec_from_webcam_faster_json_1.py

---
+++ facerec_from_webcam_faster_json_1.py
@@ -0,0 +1,90 @@
+import face_recognition
+import cv2
+import cupy as cp
+import json
+import os
+
+from raspberrypi.client import HOST_IP
+from webserver.server import HOST_IP_1
+
+class FaceRecognition():
+
+    camSet2=' tcpclientsrc host=192.168.0.2 port=8554 ! gdpdepay ! rtph264depay ! h264parse ! nvv4l2decoder  ! nvvidconv flip-method='+str(0)+' ! video/x-raw,format=BGRx ! videoconvert ! video/x-raw, width='+str(1296)+', height='+str(730)+',format=BGR ! appsink  drop=true sync=false '
+    video_capture = cv2.VideoCapture(camSet2)
+
+    known_face_encodings = []
+    known_face_names = []
+    dir = "./img_data"
+
+    # Initialize some variables
+    face_locations = []
+    face_encodings = []
+    face_names = []
+    process_this_frame = True
+
+    def read():
+        for filename in os.listdir(dir):
+            f = open(dir + "/" + filename ,"r")
+            json_data = json.load(f)
+            name = json_data["name"]
+            known_face_names.append(name)
+            enc_data = json_data["data"]
+            known_face_encodings.append(enc_data)
+    
+    def run():
+        while True:
+            # Grab a single frame of video
+            ret, frame = video_capture.read()
+
+            # Resize frame of video to 1/4 size for faster face recognition processing
+            img_gpu_src = cv2.cuda_GpuMat()
+            img_gpu_dst = cv2.cuda_GpuMat()
+
+            img_gpu_src.upload(frame)
+            img_gpu_dst = cv2.cuda.resize(img_gpu_src,(0,0), fx=0.25, fy=0.25)
+            small_frame = img_gpu_dst.download()
+
+            # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+            rgb_small_frame = small_frame[:, :, ::-1]
+
+            # Only process every other frame of video to save time
+            if process_this_frame:
+                # Find all the faces and face encodings in the current frame of video
+                face_locations = face_recognition.face_locations(rgb_small_frame)
+                face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+                face_names = []
+                for face_encoding in face_encodings:
+                    # See if the face is a match for the known face(s)
+                    matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+                    name = "Unknown"
+
+                    # # If a match was found in known_face_encodings, just use the first one.
+                    # if True in matches:
+                    #     first_match_index = matches.index(True)
+                    #     name = known_face_names[first_match_index]
+
+                    # Or instead, use the known face with the smallest distance to the new face
+                    face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+                    face_distances_gpu = cp.asarray(face_distances)
+
+                    best_match_index_gpu = cp.argmin(face_distances_gpu)
+                    best_match_index = cp.asnumpy(best_match_index_gpu)
+
+                    if matches[best_match_index]:
+                        name = known_face_names[best_match_index]
+
+                    face_names.append(name)
+                    print(name)
+
+    def terminate():
+        video_capture.release()
+        cv2.destroyAllWindows()
+
+
+
+
+if __name__ == "__main__":
+    FaceRecognition().read()
+    FaceRecognition().run()
+    FaceRecognition().terminate()
