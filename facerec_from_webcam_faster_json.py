847a74fec4ae9281de87853c4078b486c56734c0
jtmk4752 <jtmk4752@yahoo.co.jp>
2021-11-04 16:58:51 +0900

    test

--- facerec_from_webcam_faster_json.py
+++ facerec_from_webcam_faster_json.py
@@ -59,11 +59,11 @@ class FaceRecognition():
         # Only process every other frame of video to save time
         if self.process_this_frame:
 
-            print("test")
-
             # Find all the faces and face encodings in the current frame of video
             face_locations = face_recognition.face_locations(rgb_small_frame)
             face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+            print(face_locations)
 
             for face_encoding in face_encodings:
                 # See if the face is a match for the known face(s)
