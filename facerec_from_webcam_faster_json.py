9504bc8578c884c3ed0f4f3a9c09df61e59a9734
jtmk4752 <jtmk4752@yahoo.co.jp>
2021-11-05 17:12:35 +0900

    test

--- facerec_from_webcam_faster_json.py
+++ facerec_from_webcam_faster_json.py
@@ -43,8 +43,8 @@ class FaceRecognition():
         else:
             print("no video")
 
-        #face_locations = []
-        #face_encodings = []
+        face_locations = []
+        face_encodings = []
 
         # Resize frame of video to 1/4 size for faster face recognition processing
         img_gpu_src = cv2.cuda_GpuMat()
@@ -62,7 +62,7 @@ class FaceRecognition():
 
             # Find all the faces and face encodings in the current frame of video
             face_locations = face_recognition.face_locations(rgb_small_frame)
-            print(face_locations)
+            print(rgb_small_frame)
             face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
             for face_encoding in face_encodings:
