467e1df04a5954f0cc3aa4a74eca1522faf1c6e6
jtmk4752 <jtmk4752@yahoo.co.jp>
2021-11-09 14:38:51 +0900

    test

--- facerec_from_webcam_faster_json.py
+++ facerec_from_webcam_faster_json.py
@@ -67,7 +67,7 @@ class FaceRecognition():
             face_locations = face_recognition.face_locations(rgb_small_frame)
             print(face_locations)
             face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-
+            del face_locations
             for face_encoding in face_encodings:
                 # See if the face is a match for the known face(s)
                 matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
